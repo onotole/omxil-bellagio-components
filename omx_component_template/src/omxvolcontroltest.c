a9751eec5d0ab57a9f3219d22a492f6d0101d667
Giulio Urlini <giulio.urlini@st.com>
2008-08-29 12:40:40 +0000

    removed unnecessary files updated some comments
    git-svn-id: https://omxil.svn.sourceforge.net/svnroot/omxil/omxextcomp@591 d11a7513-620d-0410-9d76-e80dcb6a9808

--- omx_component_template/src/omxvolcontroltest.c
+++ omx_component_template/src/omxvolcontroltest.c
@@ -1,8 +1,7 @@
 /**
-  @file test/components/audio_effects/omxvolcontroltest.c
+  @file src/omxvolcontroltest.c
 
   This simple test application provides a testing stream for the volume control component.
-  It will be added in the more complex audio test application in the next release.
 
   Copyright (C) 2007-2008 STMicroelectronics
   Copyright (C) 2007-2008 Nokia Corporation and/or its subsidiary(-ies).
