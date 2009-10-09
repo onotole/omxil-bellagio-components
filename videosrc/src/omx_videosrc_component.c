405afea60fd1a3af0d310b3d4b62b2d94b251ce6
Giulio Urlini <giulio.urlini@st.com>
2009-10-09 12:50:12 +0000

    modified debug level for some messages
    git-svn-id: https://omxil.svn.sourceforge.net/svnroot/omxil/omxextcomp@885 d11a7513-620d-0410-9d76-e80dcb6a9808

--- videosrc/src/omx_videosrc_component.c
+++ videosrc/src/omx_videosrc_component.c
@@ -490,7 +490,7 @@ OMX_ERRORTYPE omx_videosrc_component_MessageHandler(OMX_COMPONENTTYPE* openmaxSt
   OMX_ERRORTYPE err = OMX_ErrorNone;
   OMX_STATETYPE oldState = omx_videosrc_component_Private->state;
 
-  DEBUG(DEB_LEV_SIMPLE_SEQ, "In %s\n", __func__);
+  DEBUG(DEB_LEV_FUNCTION_NAME, "In %s\n", __func__);
 
   /* Execute the base message handling */
   err = omx_base_component_MessageHandler(openmaxStandComp,message);
