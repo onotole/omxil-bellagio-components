d17fb3f14076fccce252d38ec6d7c4c6e4d1bbee
Giulio Urlini <giulio.urlini@st.com>
2010-02-04 08:59:47 +0100

    removed timed debug messages

--- examples/src/omxvideodectest.h
+++ examples/src/omxvideodectest.h
@@ -58,7 +58,6 @@ typedef struct appPrivateType{
 #define VERSIONMINOR    1
 #define VERSIONREVISION 0
 #define VERSIONSTEP     0
-long GetTime();
 
 /* Callback prototypes for video decoder */
 OMX_ERRORTYPE videodecEventHandler(
